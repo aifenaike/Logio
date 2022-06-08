0b4900fc46276425a58735494b3d5fbf9d692a03
Adedoyin <doyinadeyemi2000@gmail.com>
2022-06-08 20:29:13 +0100

    Initial log plot

---
+++ logplot/log_plot.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+
+
+class LogPlot:
+
+    def __init__(self, las_file, depth_range=tuple, plots=list):
+
+        self.dr = depth_range
+        self.plots = plots
+        self.df = las_file.df()
+        self.depth = self.df.index
+
+        self.fig, self.axes = plt.subplots(ncols=len(self.plots))
+
+        self.plot_log()
+        self.legend()
+
+
+
+
+    def plot_log(self):
+
+        # self.fig.
+        self.axes[0].set_ylabel("Depth")
+
+        for plot, axe in zip(self.plots, self.axes):
+
+            if type(plot) == tuple:
+                for plot in plot:
+                    axe.plot(self.df[plot], self.depth, lw=1.3, label=plot)
+                    axe.set_xlabel(plot)
+            else:
+                axe.plot(self.df[plot], self.depth, lw=1.3, label=plot)
+                axe.set_xlabel(plot)
+
+            if axe != self.axes[0]:
+                axe.get_yaxis().set_visible(False)
+
+            axe.set_ylim(self.dr)
+            axe.invert_yaxis()
+            axe.grid(linestyle='--', linewidth=0.4)
+
+    def legend(self):
+        for axe in self.axes:
+            axe.legend()
+
+    def show(self):
+        plt.tight_layout()
+        plt.show()
